dd94ff72986c003bc0cfedb51bff47bb8a038fa5
joan <68073738+joanitolopo@users.noreply.github.com>
2025-04-11 20:24:22 +0700

    Create bot.py
    Menambahkan file bot.py

---
+++ bot.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run("")
+
+# $heh 20
+# $hello 
+# $add 10 20
